89b10c6f504afeb0933d1de776b66dd14caffd1d
David Manuel <dmanuel@pflag.org>
2013-06-21 16:35:05 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChapterMap::Application.config.secret_token = 'e9172c043dbff906856d27724447ffc6473dd26d4118694dcd6643a1063cdca7d12fbe4c4da57caac9ff2d26aa645c4c75e1d33d94a9eeba5b99b78d6dc294e9'
