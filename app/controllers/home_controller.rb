4f523547b4f69bf980cbd07a2ea63ec3ea0c8428
David Manuel <dmanuel@pflag.org>
2013-06-24 12:52:02 -0400

    tidy embed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -34,23 +34,36 @@ class HomeController < ApplicationController
 
   def embed
     if params[:zip].nil?
-       @chapters = Chapter.all
+       @chapters = Chapter.order("state ASC").all
+       @chapters_by_state = @chapters.group_by { |t| t.state }
     else
-        @chapters = Chapter.near(params[:zip],params[:distance] || 50)
+      distance = params[:distance] unless params[:distance].nil? || params[:distance].to_f == 0 
+       @chapters = Chapter.near(params[:zip], distance || 50)
     end
      @json = @chapters.to_gmaps4rails do |chapter, marker|
         marker.infowindow render_to_string(:partial => "/chapters/infowindow", :locals => { :chapter => chapter})
-  #     marker.picture({
-  #                  :picture => "http://www.blankdots.com/img/github-32x32.png",
-  #                  :width   => 32,
-  #                  :height  => 32
-  #                 })
         marker.title   "#{chapter.name}"
-        marker.json({ :id => chapter.id})
+        marker.json({ :id => chapter.id })
+        case chapter.category
+        when "Chapter"
+          marker.picture({
+                    :picture => "http://maps.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png",
+                    :width   => 32,
+                    :height  => 32
+                   });
+        when "Representative"
+          marker.picture({
+                    :picture => "http://maps.google.com/intl/en_us/mapfiles/ms/micons/purple-dot.png",
+                    :width   => 32,
+                    :height  => 32
+                   });
+        end
 
     end
 
-    render :layout => 'embed'    
+    @circles_json = @chapters.map {|c| {:lat => c.latitude, :lng => c.longitude, :radius => c.radius || 100000 }}.to_json
+
+    render 'index', :layout => 'embed'    
 
   end
 
