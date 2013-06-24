34aa9b8dbb9d1d301230ccf20a424fcc45abb9d2
David Manuel <dmanuel@pflag.org>
2013-06-24 10:07:27 -0400

    ein display

--- db/schema.rb
+++ db/schema.rb
@@ -14,38 +14,37 @@
 ActiveRecord::Schema.define(:version => 20130624133941) do
 
   create_table "chapters", :force => true do |t|
-    t.string   "name"
-    t.string   "website"
-    t.string   "street"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zip"
-    t.string   "email_1"
-    t.string   "email_2"
-    t.string   "email_3"
-    t.string   "helpline"
-    t.string   "phone_1"
-    t.string   "phone_2"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.string   "ein"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.boolean  "gmaps"
-    t.string   "gmaps_address"
-    t.integer  "radius"
-    t.string   "type"
-    t.string   "category"
+    t.string    "name"
+    t.string    "website"
+    t.string    "street"
+    t.string    "city"
+    t.string    "state"
+    t.string    "zip"
+    t.string    "email_1"
+    t.string    "email_2"
+    t.string    "email_3"
+    t.string    "helpline"
+    t.string    "phone_1"
+    t.string    "phone_2"
+    t.float     "latitude"
+    t.float     "longitude"
+    t.string    "ein"
+    t.timestamp "created_at",    :null => false
+    t.timestamp "updated_at",    :null => false
+    t.boolean   "gmaps"
+    t.string    "gmaps_address"
+    t.integer   "radius"
+    t.string    "category"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.boolean  "admin"
+    t.string    "name"
+    t.string    "email"
+    t.string    "provider"
+    t.string    "uid"
+    t.timestamp "created_at", :null => false
+    t.timestamp "updated_at", :null => false
+    t.boolean   "admin"
   end
 
 end
