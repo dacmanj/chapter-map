85913d6ff0c0fba4601c063643f9553a71eba2b6
David Manuel <dmanuel@pflag.org>
2013-07-04 18:42:13 -0400

    state search
    Signed-off-by: David Manuel <dmanuel@pflag.org>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < ApplicationController
   def index
-    if params[:zip].blank?
-       @chapters = Chapter.active.order("state ASC").all
-       @chapters_by_state = @chapters.group_by { |t| t.state }
-    else
+    if !params[:state].blank?
+      @chapters = Chapter.where("state is ?",params[:state]).order("name ASC")
+
+    elsif !params[:zip].blank?
       distance = params[:distance] unless params[:distance].blank? || params[:distance].to_f == 0 
-       @chapters = Chapter.active.near(params[:zip], distance || 50)
+      @chapters = Chapter.active.near(params[:zip], distance || 50)
+    else
+      @chapters = Chapter.active.order("state ASC, name ASC").all
+      @chapters_by_state = @chapters.group_by { |t| t.state }
     end
      @json = @chapters.to_gmaps4rails do |chapter, marker|
         marker.infowindow render_to_string(:partial => "/chapters/infowindow", :locals => { :chapter => chapter})
@@ -33,38 +36,9 @@ class HomeController < ApplicationController
   end
 
   def embed
-    if params[:zip].blank?
-       @chapters = Chapter.active.order("state ASC").all
-       @chapters_by_state = @chapters.group_by { |t| t.state }
-    else
-      distance = params[:distance] unless params[:distance].blank? || params[:distance].to_f == 0 
-       @chapters = Chapter.active.near(params[:zip], distance || 50)
-    end
-     @json = @chapters.to_gmaps4rails do |chapter, marker|
-        marker.infowindow render_to_string(:partial => "/chapters/infowindow", :locals => { :chapter => chapter})
-        marker.title   "#{chapter.name}"
-        marker.json({ :id => chapter.id })
-        case chapter.category
-        when "Chapter"
-          marker.picture({
-                    :picture => "http://maps.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png",
-                    :width   => 32,
-                    :height  => 32
-                   });
-        when "Representative"
-          marker.picture({
-                    :picture => "http://maps.google.com/intl/en_us/mapfiles/ms/micons/purple-dot.png",
-                    :width   => 32,
-                    :height  => 32
-                   });
-        end
-
-    end
-
-    #@circles_json = @chapters.map {|c| {:lat => c.latitude, :lng => c.longitude, :radius => c.radius || 100000 }}.to_json
-
+    index #call index
     render 'index', :layout => 'embed'    
-
+    #@circles_json = @chapters.map {|c| {:lat => c.latitude, :lng => c.longitude, :radius => c.radius || 100000 }}.to_json
   end
 
 end
