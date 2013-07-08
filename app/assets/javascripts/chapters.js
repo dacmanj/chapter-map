d09952de452e27cfe6c8173cbd7cc68502ed2e19
David Manuel <dmanuel@pflag.org>
2013-07-08 14:15:09 -0400

    css fixes, full state name in listings

--- app/assets/javascripts/chapters.js
+++ app/assets/javascripts/chapters.js
@@ -1,10 +1,18 @@
 
 $().ready(function() {
 	$("#geocode").change(function() {if ($("#geocode").prop("checked")) { $("#chapter_latitude").val("").attr("readonly","true"); $("#chapter_longitude").val("").attr("readonly","true"); } else { $("#chapter_latitude").removeAttr("readonly"); $("#chapter_longitude").removeAttr("readonly");}})
-	$("body.home.index #state").change(submitChapterSearch);
-	$("body.home.index #distance").change(submitChapterSearch);
-
+	$("body.home #state").change(submitChapterSearch);
+	$("body.home #distance").change(submitChapterSearch);
+	$("a.btn.reset-search").click(clearSearch);
 });
+
+function clearSearch(e) {
+	var form = $(this).parent()[0]
+	form.reset();
+	$("select",form).val("")
+	form.submit();
+	return false;
+}
 
 function submitChapterSearch() {
 	(($("#state option:selected").val() != "") || ($("#zip").val() != "")) && $("form.chapter-search").submit();
