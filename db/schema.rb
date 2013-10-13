00f88ddc56cd6bf14e448af16c5d44cc2afd956e
David Manuel <dmanuel@pflag.org>
2013-10-12 22:40:12 -0400

    add papertrail to chapters

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130919161852) do
+ActiveRecord::Schema.define(:version => 20131013022923) do
 
   create_table "Attachments", :force => true do |t|
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
-    t.string   "attachment_file_name"
-    t.string   "attachment_content_type"
-    t.integer  "attachment_file_size"
-    t.datetime "attachment_updated_at"
-    t.integer  "chapter_id"
-    t.string   "tag"
-  end
-
-  create_table "assets", :force => true do |t|
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
     t.string   "attachment_file_name"
@@ -149,5 +138,17 @@ ActiveRecord::Schema.define(:version => 20130919161852) do
     t.string   "password_digest"
     t.string   "activation_code"
   end
+
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",      :null => false
+    t.integer  "item_id",        :null => false
+    t.string   "event",          :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+    t.text     "object_changes"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
