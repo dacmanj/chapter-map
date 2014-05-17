370bc7214e292acda826c66d70c2acc2f15a2f0c
David Manuel <dmanuel@pflag.org>
2014-05-17 13:14:29 -0400

    invalid t/f import check

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -202,8 +202,11 @@ class Chapter < ActiveRecord::Base
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
     when ".csv" then Roo::CSV.new(file.path)
+    when ".CSV" then Roo::CSV.new(file.path)
     when ".xls" then Roo::Excel.new(file.path)
+    when ".XLS" then Roo::Excel.new(file.path)
     when ".xlsx" then Roo::Excelx.new(file.path)
+    when ".XLSX" then Roo::Excelx.new(file.path)
     else raise "Unknown file type: #{file.original_filename}"
     end
   end
